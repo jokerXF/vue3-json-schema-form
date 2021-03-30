cb88cb99322e6eb50a0cafcd4acf41d3d939ea4e
jokerXF <2276556336@qq.com>
2021-03-30 16:38:50 +0800

    h function

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
+import { createApp, defineComponent, h } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+//import App from './App.vue'
+const img = require('./assets/logo.png')//eslint-disable-line
+//import image from './assets/logo.png'
+const App = defineComponent({
+  render() {
+    return h('div', { id: 'app' }, [
+      h('img', {
+        alt: 'vue logo',
+        src: img,
+      }),
+      h(HelloWorld, {
+        msg: 'Welcome to Your Vue.js + TypeScript App',
+      }),
+    ])
+  },
+})
 createApp(App).mount('#app')
